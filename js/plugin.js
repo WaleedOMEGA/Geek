f090a59bf72ad66543545619e15b4ae5f713e9fe
Waleed OMEGA <waleedomega@hotmail.com>
2020-08-05 13:50:04 +0200

    Add files via upload

---
+++ js/plugin.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    "use strict";
+    
+    // Nice Scroll
+    
+    $("html").niceScroll();
+    
+    $('.carousel').carousel({
+        
+        interval: 6000
+        
+    });
+});
